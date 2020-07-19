c54ae6c1c5e97033e2f5bbb03f7ec301fd7fc463
adam080880 <muhamadadam20@gmail.com>
2020-07-19 20:59:23 +0700

    fix conflict

--- src/redux/actions/users.js
+++ src/redux/actions/users.js
@@ -1,18 +1,26 @@
-import http from '../../services/http'
-import httpForm from '../../services/httpForm'
-import { API } from 'react-native-dotenv'
+import http from '../../services/http';
+import httpForm from '../../services/httpForm';
+import {API} from 'react-native-dotenv';
+
+const getUserById = (id, token) => {
+  const url = `${API}user/${id}`;
+  return {
+    type: 'GETUSERID',
+    payload: http().get(url),
+  };
+};
 
 const getUser = (param) => {
-  const url = `${API}user?${param}`
-  console.log(url)
+  const url = `${API}user?${param}`;
+  console.log(url);
   return {
     type: 'GETUSER',
     payload: http().get(url),
   };
 };
 
-const patchUser = (dataUser, token) =>{
-  const url = `${API}profile`
+const patchUser = (dataUser, token) => {
+  const url = `${API}profile`;
   return {
     type: 'PATCHUSER',
     payload: httpForm(token).patch(url, dataUser),
@@ -20,8 +28,8 @@ const patchUser = (dataUser, token) =>{
 };
 
 const deleteUser = (id) => {
-  const url = `${API}user/${id}`
-  console.log(url)
+  const url = `${API}user/${id}`;
+  console.log(url);
   return {
     type: 'DELETEUSER',
     payload: http().delete(url),
